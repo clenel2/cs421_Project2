59dfa605baad13b422c4182c1ead38bdd95d394d
clenell <47303706+clenel2@users.noreply.github.com>
2020-10-14 17:20:35 -0500

    First try at Virtual button

--- Assets/Trans.cs
+++ Assets/Trans.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class Trans : MonoBehaviour
 {
 	Animator anim;
+    GameObject vBtn;
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        vBtn = GameObject.Find("VirtualButton");
+        vBtn.GetComponent<VirtualButtonBehaviour>();
+
     }
 
+    void OnButtonPressed(VirtualButton vb)
+    {
+        anim.SetTrigger("Active");
+    }
     // Update is called once per frame
     void Update()
-    {
+    {  
         if (Input.GetMouseButtonDown(0)) 
         {
         	anim.SetTrigger("Active");
